0a22fe1923d2f688b33a3f47625af3bed741424c
Lee Shadle <leeshadle@gmail.com>
2020-05-06 18:01:14 -0700

    building controls module

---
+++ packages/controls/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require( 'path' );
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const defaultPlugins = defaultConfig.plugins;
+
+/* Plugins */
+
+// Compile block frontend and editor scss files into css files.
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const extractStyles = new ExtractTextPlugin( './style.css' );
+const extractEditorStyles = new ExtractTextPlugin( './editor.css' );
+
+const config = {
+	...defaultConfig,
+	mode: 'production',
+	devtool: 'source-map',
+	entry: {
+		index: [ path.resolve( process.cwd(), `src/index.js` ) ],
+	},
+	output: {
+		path: path.resolve( process.cwd(), `./build` ),
+		filename: '[name].js',
+	},
+	module: {
+		...defaultConfig.module,
+		rules: [
+			...defaultConfig.module.rules,
+			{
+				test: /editor\.(sa|sc|c)ss$/,
+				use: extractEditorStyles.extract( {
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: false,
+							},
+						},
+					],
+				} ),
+			},
+			{
+				test: /style\.(sa|sc|c)ss$/,
+				use: extractStyles.extract( {
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: false,
+								prependData:
+									'@import "./src/assets/scss/common.scss";\n',
+							},
+						},
+					],
+				} ),
+			},
+		],
+	},
+	plugins: [ ...defaultPlugins, extractStyles, extractEditorStyles ],
+};
+
+module.exports = config;
