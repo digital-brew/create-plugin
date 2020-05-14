f8311ce0cb8276c76cff25f739433cd7dec4e90c
Lee Shadle <leeshadle@gmail.com>
2020-05-13 18:28:44 -0700

    updating bordercontrols

--- esnext-example/src/blocks/inner-block/icon.js
+++ esnext-example/src/blocks/inner-block/icon.js
@@ -13,21 +13,40 @@
 const icon = <svg viewBox="0 0 104 104">
     <defs>
       <rect x="0" y="0" width="100" height="100" />
+      <polygon points="0 0 100 0 100 100 0 100 0 71.6914062" />
     </defs>
     <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <g transform="translate(2.000000, 2.000000)">
         <g>
+          <g>
+            <mask fill="white">
+              <use xlinkHref="#path-1" />
+            </mask>
+            <use stroke="#FFFFFF" strokeWidth="3" xlinkHref="#path-1" />
+            <rect
+              stroke="currentColor"
+              strokeWidth="10"
+              mask="url(#mask-2)"
+              x="-39"
+              y="-39"
+              width="118"
+              height="118"
+              rx="38"
+            />
+          </g>
           <mask fill="white">
-            <use xlinkHref="#path-1" />
+            <use xlinkHref="#path-3" />
           </mask>
-          <use stroke="#FFFFFF" strokeWidth="3" xlinkHref="#path-1" />
+          <use stroke="#FFFFFF" strokeWidth="3" xlinkHref="#path-3" />
           <path
-            d="M16,16 L84,16 L84,84 L16,84 L16,16 Z"
-            stroke="#000000"
-            strokeWidth="3"
+            d="M29,-54 L97,-54 L97,14 L29,14 L29,-54 Z"
             fill="#FFFFFF"
-            strokeDasharray="6"
-            mask="url(#mask-2)"
+            mask="url(#mask-4)"
+          />
+          <path
+            d="M-54,50 L14,50 L14,118 L-54,118 L-54,50 Z"
+            fill="#FFFFFF"
+            mask="url(#mask-4)"
           />
         </g>
       </g>
--- packages/controls/src/border-controls/icons.js
+++ packages/controls/src/border-controls/icons.js
@@ -1,28 +1,26 @@
 const icons = {};
 
 icons.borderRadiusNone = <svg viewBox="0 0 104 104">
-<defs>
-	<rect x="0" y="0" width="100" height="100" />
-</defs>
-<g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-	<g transform="translate(2.000000, 2.000000)">
-		<g>
-			<mask fill="white">
-				<use xlinkHref="#path-1" />
-			</mask>
-			<use stroke="#FFFFFF" strokeWidth="3" xlinkHref="#path-1" />
-			<path
-				d="M16,16 L84,16 L84,84 L16,84 L16,16 Z"
-				stroke="#000000"
-				strokeWidth="3"
-				fill="#FFFFFF"
-				strokeDasharray="6"
-				mask="url(#mask-2)"
-			/>
-		</g>
-	</g>
-</g>
-</svg>;
+    <defs>
+      <rect x="0" y="0" width="100" height="100" />
+    </defs>
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g transform="translate(2.000000, 2.000000)">
+        <g>
+          <mask fill="white">
+            <use xlinkHref="#path-1" />
+          </mask>
+          <use stroke="#FFFFFF" strokeWidth="3" xlinkHref="#path-1" />
+          <path
+            d="M16,16 L84,16 L84,84 L16,84 L16,16 Z"
+            fill="#FFFFFF"
+            mask="url(#mask-2)"
+          />
+        </g>
+      </g>
+    </g>
+  </svg>;
+
 
 icons.borderRadiusSmall = <svg viewBox="0 0 104 104"><defs><rect height="100" width="100" x="0" y="0"/></defs><g fill="none" stroke="none" strokeWidth="1" fillRule="evenodd"><g transform="matrix(1 0 0 1 2 2)"><g><mask fill="#fff"><use xlinkHref="#path-1"/></mask><use stroke="#fff" strokeWidth="3" xlinkHref="#path-1"/><rect height="118" rx="2" stroke="currentColor" width="118" x="-51" y="-48" strokeWidth="10"/></g></g></g></svg>;
 
@@ -44,6 +42,47 @@ icons.borderWidthXLarge = <svg viewBox="0 0 104 104"><defs><rect height="100" wi
 
 icons.borderWidth = <svg fill="currentColor" height="20px" version="1.1" viewBox="0 0 24 24" width="20px" x="0px" y="0px"><title>content_040-no_border-table-cell-square</title><desc>Created with Sketch.</desc><g fill="none" stroke="none" strokeWidth="1" fillRule="evenodd"><g fill="currentColor" transform="matrix(1 0 0 1 -476 -188)"><g transform="matrix(1 0 0 1 477 189)"><rect height="1" width="2" x="0" y="0"/><rect height="1" width="2" x="8" y="0"/><rect height="1" width="2" x="4" y="0"/><rect height="1" width="2" x="12" y="0"/><rect height="2" width="1" x="0" y="7"/><rect height="2" width="1" x="0" y="3"/><rect height="2" width="1" x="20" y="8"/><rect height="2" width="1" x="20" y="0"/><rect height="2" width="1" x="20" y="4"/><rect height="2" width="1" x="0" y="11"/><rect height="2" width="1" x="0" y="15"/><rect height="2" width="1" x="0" y="19"/><rect height="2" width="1" x="20" y="12"/><rect height="2" width="1" x="20" y="16"/><rect height="1" width="2" x="19" y="20"/><rect height="1" width="2" x="3" y="20"/><rect height="1" width="2" x="7" y="20"/><rect height="1" width="2" x="11" y="20"/><rect height="1" width="2" x="16" y="0"/><rect height="1" width="2" x="15" y="20"/></g></g></g></svg>;
 
-icons.borderRadius = <svg height="20px" viewBox="0 0 66 66" width="20px"><path d="m457 596.97658v5.02342c0 .552285-.447715 1-1 1h-12c-.552285 0-1-.447715-1-1v-5h-14.996435c-6.077067 0-11.003565-4.926533-11.003565-11.003565v-37.99287c0-6.077067 4.926533-11.003565 11.003565-11.003565h37.99287c6.077067 0 11.003565 4.926533 11.003565 11.003565v14.996435h5c.552285 0 1 .447715 1 1v12c0 .552285-.447715 1-1 1h-5.023422c-.506685 10.803114-9.172511 19.469882-19.976578 19.97658zm-12 4.02342h10v-10h-10zm26-26h10v-10h-10zm-28 20v-5c0-.552285.447715-1 1-1h12c.552285 0 1 .447715 1 1v4.974104c9.699148-.5031 17.471015-8.275803 17.974102-17.974104h-4.974102c-.552285 0-1-.447715-1-1v-12c0-.552285.447715-1 1-1h5v-14.996435c0-4.972466-4.03107-9.003565-9.003565-9.003565h-37.99287c-4.972466 0-9.003565 4.03107-9.003565 9.003565v37.99287c0 4.972466 4.03107 9.003565 9.003565 9.003565z" transform="matrix(1 0 0 1 -417 -537)" fillRule="evenodd"/></svg>;
+icons.borderRadius = <svg viewBox="0 0 104 104">
+<defs>
+	<rect x="0" y="0" width="100" height="100" />
+	<polygon points="0 0 100 0 100 100 0 100 0 71.6914062" />
+</defs>
+<g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+	<g transform="translate(2.000000, 2.000000)">
+		<g>
+			<g>
+				<mask fill="white">
+					<use xlinkHref="#path-1" />
+				</mask>
+				<use stroke="#FFFFFF" strokeWidth="3" xlinkHref="#path-1" />
+				<rect
+					stroke="currentColor"
+					strokeWidth="10"
+					mask="url(#mask-2)"
+					x="-39"
+					y="-39"
+					width="118"
+					height="118"
+					rx="38"
+				/>
+			</g>
+			<mask fill="white">
+				<use xlinkHref="#path-3" />
+			</mask>
+			<use stroke="#FFFFFF" strokeWidth="3" xlinkHref="#path-3" />
+			<path
+				d="M29,-54 L97,-54 L97,14 L29,14 L29,-54 Z"
+				fill="#FFFFFF"
+				mask="url(#mask-4)"
+			/>
+			<path
+				d="M-54,50 L14,50 L14,118 L-54,118 L-54,50 Z"
+				fill="#FFFFFF"
+				mask="url(#mask-4)"
+			/>
+		</g>
+	</g>
+</g>
+</svg>;
 
 export default icons;
